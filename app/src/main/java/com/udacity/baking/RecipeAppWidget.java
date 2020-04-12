84fba7645a69027f2a33e05bea7d445e546bb314
aK <l@noreply.com>
2020-04-12 16:07:40 -0700

    Update widget to display Recipe name, ingredients with measurement

--- app/src/main/java/com/udacity/baking/RecipeAppWidget.java
+++ app/src/main/java/com/udacity/baking/RecipeAppWidget.java
@@ -3,9 +3,14 @@ package com.udacity.baking;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.RemoteViews;
+
+import com.udacity.baking.database.RecipeIngredientsEntity;
+import com.udacity.baking.viewmodel.DetailViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,20 +20,60 @@ import java.util.List;
  */
 public class RecipeAppWidget extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+
+    private static DetailViewModel mDetailViewModel;
+    private static final String TAG = "WIDGET - INGREDIENTS";
+    private static final String INGREDIENTS = "Ingredients";
+
+    private String mRecipeName = "";
+    private List<RecipeIngredientsEntity> mRecipeIngredients = new ArrayList<>();
+
+
+     void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
 
-        // TODO: move to be dynamically changed when recipe is selected
+
+
+
 
         String ingredientName, ingredientMeasurement;
 
         ingredientName = "Egg";
         ingredientMeasurement = "1 dozen";
 
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_app_widget);
-        views.setTextViewText(R.id.widget_text_ingredientName, ingredientName);
-        views.setTextViewText(R.id.widget_text_ingredient_measurement, ingredientMeasurement);
+
+
+        // Create a list for ingredients
+         StringBuilder ingredientBuilder = new StringBuilder();
+         StringBuilder measurementBuilder = new StringBuilder();
+
+         for (int i = 0; i < mRecipeIngredients.size(); i++) {
+             String currentIngredient = mRecipeIngredients.get(i).getIngredient();
+             String currentMeasurement = String.valueOf(mRecipeIngredients.get(i).getQuantity());
+             currentMeasurement = currentMeasurement + " " + mRecipeIngredients.get(i).getMeasure();
+
+             ingredientBuilder.append(currentIngredient + "\n");
+             measurementBuilder.append(currentMeasurement + "\n");
+         }
+
+
+
+
+         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_app_widget);
+
+
+
+         // Update widget details if there's data
+         if(mRecipeIngredients != null){
+            //views.setTextViewText(R.id.widget_text_ingredientName, mRecipe);
+            views.setTextViewText(R.id.widget_text_ingredient_measurement, measurementBuilder.toString());
+            views.setTextViewText(R.id.widget_text_ingredientName, ingredientBuilder.toString());
+
+            mRecipeName = mRecipeName + " " + INGREDIENTS;
+            views.setTextViewText(R.id.widget_text_header, mRecipeName);
+
+        }
 
 
 
@@ -42,21 +87,47 @@ public class RecipeAppWidget extends AppWidgetProvider {
 
 
 
+
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
 
     }
+
+
+
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
 
-
-
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
     }
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+        if(intent != null) {
+            mRecipeIngredients = (List<RecipeIngredientsEntity>) intent.getSerializableExtra("INGREDIENTS");
+            mRecipeName = intent.getStringExtra("recipeName");
+
+            Log.d(TAG, "onReceive: " + mRecipeIngredients.get(0).getIngredient());
+
+
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+
+            ComponentName componentName = new ComponentName(context, RecipeAppWidget.class);
+            appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetManager.getAppWidgetIds(componentName), R.id.widget_text_ingredientName);
+            this.onUpdate(context,appWidgetManager,appWidgetManager.getAppWidgetIds(componentName));
+        }
+
+    }
+
+
+
 
     @Override
     public void onEnabled(Context context) {
